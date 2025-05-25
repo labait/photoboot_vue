e396d0fcb5ca3ec6a6f2fdf5b7d5274f77a2332a
Antonio Molinari <molinari@incode.it>
2025-05-25 23:33:04 +0200

    Add logo asset: introduce logo_laba.svg to assets and update Header.vue to reference the new logo path, enhancing visual branding in the application.

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -13,7 +13,7 @@ defineProps({
 <template>
     <router-link to="/" class="text-white mb-8 flex items-center">
         <div class="flex flex-col items-center">
-            <img src="../../images/logo_laba.svg" alt="Logo" class="w-48 mb-8">
+            <img src="../assets/logo_laba.svg" alt="Logo" class="w-48 mb-8">
             <h1 class="text-5xl font-bold">{{ title }}</h1>
         </div>
     </router-link>
