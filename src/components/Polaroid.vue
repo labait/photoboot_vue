8f07ddfaed18d39a73408fd1ce596d4b4075ff19
Antonio Molinari <molinari@incode.it>
2025-05-25 23:25:57 +0200

    Add Polaroid component: create Polaroid.vue file with template, script, and styles for a new visual element in the application.

---
+++ src/components/Polaroid.vue
@@ -0,0 +1,34 @@
+<script setup>
+import { ref,defineProps } from 'vue';
+
+</script>
+
+<template>
+    <div class="polaroid">
+       <div class="polaroid-inner">
+            <slot /> 
+       </div>
+    </div>
+</template>
+
+<style scoped>
+.polaroid {
+    --polaroid-width: 420px;
+    --polaroid-height: 380px;
+    --polaroid-padding: 20px;
+}
+.polaroid {
+    width: var(--polaroid-width);
+    height: var(--polaroid-height);
+    background-color: #fff;
+    box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+    .polaroid-inner {
+        margin: var(--polaroid-padding);
+        width: calc(var(--polaroid-width) - var(--polaroid-padding) * 2);
+        height: calc(var(--polaroid-height) - var(--polaroid-padding) * 4);
+        background-color: red;
+        overflow: hidden;
+        position: relative;
+    }   
+}
+</style>
