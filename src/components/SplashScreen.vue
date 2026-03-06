4e30ca68b6ed1ed7c4a7187c2e8059e0f219d5cc
Federica Milesi <fede.arctic@gmail.com>
2026-03-06 09:48:16 +0100

    feat: add mobile background image and improve splash screen  responsive

--- src/components/SplashScreen.vue
+++ src/components/SplashScreen.vue
@@ -7,28 +7,25 @@ const config = inject('config');
 </script>
 
 <template>
-  <div class="px-16">
+  <div class="px-4 sm:px-8 md:px-16 min-h-screen flex flex-col justify-center">
     <Header title="Benvenut*" />
-
     <div>
-      <p class="text-white font-bold text-2xl sm:text-3xl md:text-4xl lg:text-6xl mb-8">
+      <p class="text-white font-bold text-2xl sm:text-3xl md:text-4xl lg:text-6xl mb-6 md:mb-8">
         LABA's photobooth
       </p>
-      
-      <div class="flex items-center gap-4 mb-8">
-        <img src="../assets/txt.svg" class="w-1/2 h-auto object-cover block">
-        <img src="../assets/polaroid.svg" class="w-1/2 h-auto object-cover block">
+      <div class="flex flex-col sm:flex-row items-center gap-4 mb-6 md:mb-8">
+        <img src="../assets/txt.svg" class="w-full sm:w-1/2 h-auto object-cover block">
+        <img src="../assets/polaroid.svg" class="w-full sm:w-1/2 h-auto object-cover block w-[0.8]">
       </div>
     </div>
-
-    <div class="flex space-x-4 relative z-20 items-center justify-start"> 
-      <router-link v-if="config.features.camera" to="/cam" class="btn-primary rounded-full bg-[#FF7230] text-white w-fit">
+    <div class="flex relative z-20 items-center justify-start">
+      <router-link
+        v-if="config.features.camera"
+        to="/cam"
+        class="btn-primary rounded-full bg-[#FF7230] text-white w-fit px-6 py-3 text-sm sm:text-base"
+      >
         Scopri l'esperienza
       </router-link>
     </div>
   </div>
-    <!-- <router-link v-if="config.features.list" to="/list" class="btn-primary">
-      Vai alla raccolta
-    </router-link>  -->
-
 </template>
