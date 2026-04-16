5d3ca33a8680e66917e2813ce3f82d098e41155c
Federica Milesi <fede.arctic@gmail.com>
2026-04-16 15:04:25 +0200

    feat: update Posters component to use local posters.json and enhance layout

--- src/components/Posters.vue
+++ src/components/Posters.vue
@@ -4,7 +4,7 @@ import { useRouter } from 'vue-router'
 
 const router = useRouter()
 
-import posters from '../../public/posters/posters.json'
+import posters from '../../src/posters.json'
 //randomize posters
 posters.sort(() => Math.random() - 0.5)
 
@@ -14,22 +14,31 @@ const global = inject('global')
 <template>
   {{ global }}
   <div class="posters-container container mx-auto max-w-7xl">
+
     <h1>Posters</h1>
-    <div class="posters grid grid-cols-1 md:grid-cols-4 lg:grid-cols-8 gap-4">
-      <a 
-        class="poster cursor-pointer bg-white rounded-lg p-4 flex flex-col items-center justify-center" 
-        v-for="poster in posters" :key="poster.name"
+
+    <div class="posters grid grid-cols-5 gap-4 mb-4">
+      
+        <a class="poster cursor-pointer bg-white rounded-lg p-4 flex flex-col items-center "
+        v-for="poster in posters.slice(0, 10)" :key="poster.name"
         @click="() => {
           global.poster = poster;
           router.push('/cam');
         }"
-      > 
-        <div class="title text-xl font-bold mb-2">{{ poster.name }}</div>
-        <img :src="`/posters/${poster.file_path}`" :alt="poster.name" class="w-full h-full object-cover" />
+      >
+        <div class="title text-lg font-bold mb-2 text-left w-full h-16">{{ poster.name }}</div>
+        <div class="w-full h-64 overflow-hidden rounded">
+          <img :src="`/posters/${poster.file_path}`" :alt="poster.name" class="w-full h-full object-cover" />
+        </div>
       </a>
     </div>
-  </div>
-</template>
+
+    </div>
+  </template>
+
+<style scoped>
+
+</style>
 
 <style scoped>
 
