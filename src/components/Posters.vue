c6f05bdfbc5df0a85d762ba1f72353d0da276f63
Federica Milesi <fede.arctic@gmail.com>
2026-04-16 15:59:52 +0200

    feat: enhance Posters component layout and styling for improved user experience

--- src/components/Posters.vue
+++ src/components/Posters.vue
@@ -12,21 +12,22 @@ const global = inject('global')
 </script>
 
 <template>
-  {{ global }}
+  <!--{{ global }} -->
   <div class="posters-container container mx-auto max-w-7xl">
+  <div class="h-35 flex items-center justify-center">
+    <h1 class="text-white font-bold text-[6vw] sm:text-[2vw] py-2">Seleziona il tuo poster preferito: </h1>
+    </div>
 
-    <h1>Posters</h1>
-
-    <div class="posters grid grid-cols-5 gap-4 mb-4">
+    <div class="posters grid grid-cols-5 gap-4 mb-4 ">
       
-        <a class="poster cursor-pointer bg-white rounded-lg p-4 flex flex-col items-center "
+        <a class="poster cursor-pointer border bg-[#4f485f] rounded-lg p-4 flex flex-col items-center hover:bg-[#7069a2] hover:scale-103 transition-transform duration-300"
         v-for="poster in posters.slice(0, 10)" :key="poster.name"
         @click="() => {
           global.poster = poster;
           router.push('/cam');
         }"
       >
-        <div class="title text-lg font-bold mb-2 text-left w-full h-16">{{ poster.name }}</div>
+        <div class="title text-lg font-bold mb-2 text-left w-full h-16 text-white">{{ poster.name }}</div>
         <div class="w-full h-64 overflow-hidden rounded">
           <img :src="`/posters/${poster.file_path}`" :alt="poster.name" class="w-full h-full object-cover" />
         </div>
